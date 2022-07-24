4221fc9dd1b06ad20f8a9322d6e450659bdc87e4
Frann0 <deengo113@gmail.com>
2022-07-24 22:24:51 +0200

    Navbar login

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/landingPage/landingPage';
+import Navbar from './components/shared/navbar/Navbar';
 
 function App() {
+  const routes = [
+    { path: "/", element: <LandingPage /> },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={
+              <>
+                <Navbar/>
+                {route.element}
+              </>
+            } />
+          ))}
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/landingPage/landingPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './landingPage.scss'
+
+const LandingPage = () => {
+  return (
+    <div className='test'>landingPage</div>
+  )
+}
+
+export default LandingPage
