159892581973a139b6fcf7ae63c798ca387a9269
ceci6182 <ceci6182@easv365.dk>
2022-08-03 16:26:17 +0200

    functionality

--- src/stores/userStore.tsx
+++ src/stores/userStore.tsx
@@ -1,9 +1,11 @@
 import { action, makeAutoObservable, observable } from "mobx";
-import {CreateUserDTO, UserDTO } from "../models/user/userInterface";
+import {CreateUserDTO, LoginDTO, LoginResponseDTO, UserDTO} from "../models/user/userInterface";
 import userService from "../services/userService";
 
-export class UserStore{
+export class UserStore {
+
     @observable user: UserDTO | undefined;
+    @observable loginResponse: LoginResponseDTO | undefined;
 
     constructor() {
         makeAutoObservable(this);
@@ -13,5 +15,21 @@ export class UserStore{
     create = async (data: CreateUserDTO) =>{
         const response = await userService.createUser(data)
         this.user = response.data
+    }
+
+    @action
+    getById = async (userId: string) =>{
+        const response = await userService.getById(userId)
+        this.user = response.data
+    }
+
+    @action
+    login = async(data: LoginDTO) => {
+        const response = await userService.login(data)
+        this.loginResponse = response.data;
+        console.log(this.loginResponse)
+        if(this.loginResponse?.isValid){
+            this.getById(this.loginResponse.userId)
+        }
     }
 }
