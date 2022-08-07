e982fe368e4e0cdc0441797e6878db11748516ec
Frann0 <deengo113@gmail.com>
2022-08-07 17:45:46 +0200

    Fixed autofill issues

--- src/pages/landingPage/landingPage.tsx
+++ src/pages/landingPage/landingPage.tsx
@@ -1,5 +1,7 @@
 import { observer } from 'mobx-react-lite';
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
 import { useStore } from '../../stores/store';
 import './landingPage.scss'
 
@@ -7,10 +9,11 @@ const LandingPage = () => {
     const [loaded, setLoaded] = useState(false);
     const [hasPin, setHasPin] = useState(false);
     const [pinValue, setPinValue] = useState('');
+    const navigate = useNavigate();
     /**
      * Example of how to use the store context
      */
-    const { authStore } = useStore();
+    const { userStore } = useStore();
 
     useEffect(() => {
         setLoaded(true);
@@ -23,6 +26,18 @@ const LandingPage = () => {
 
     const checkPinLength = () => {
         setHasPin(pinValue.length > 0);
+    }
+
+    const handleJoinClick = () => {
+        if (userStore.user === undefined) {
+            navigate('/login');
+        }
+    }
+
+    const handleHostClick = () => {
+        if (userStore.user === undefined) {
+            navigate('/login');
+        }
     }
 
     return (
@@ -38,9 +53,9 @@ const LandingPage = () => {
                             <div className={`LandingPage-JoinInput ${hasPin ? "active" : ""}`} >
                                 <input type="text" placeholder='Pin Code' maxLength={5} onChange={(e) => handlePinChange(e)} onKeyUp={() => checkPinLength()} />
                             </div>
-                            <div className='LandingPage-JoinButton'>Join</div>
+                            <div className='LandingPage-JoinButton' onClick={() => handleJoinClick()}>Join</div>
                         </div>
-                        <div className='LandingPage-CreateRoom'>
+                        <div className='LandingPage-CreateRoom' onClick={() => handleHostClick()}>
                             Create Room
                         </div>
                     </div>
