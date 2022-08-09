55a3902bfc5fe56e49318c3161222b2b66026324
Frann0 <50768854+Frann0@users.noreply.github.com>
2022-08-09 13:26:46 +0200

    Game store

---
+++ src/models/game/gameInterfaces.tsx
@@ -0,0 +1,31 @@
+import { TileForUser } from "../tile/tileInterface";
+import { UserDTO } from "../user/userInterface";
+
+export interface Lobby {
+    host: UserDTO
+    players: UserDTO[]
+    inProgress: boolean
+    settings: Settings
+    tiles: TileForUser[]
+    pin: string
+}
+
+export interface GameRoom {
+    boards: Board[]
+    status: boolean
+    lobby: Lobby
+}
+
+export interface Board {
+    owner: UserDTO
+    tiles: GameTile[]
+}
+
+export interface GameTile {
+    tile: TileForUser
+    completed: boolean
+}
+
+export interface Settings {
+
+}
