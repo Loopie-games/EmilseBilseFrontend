6bcf1f8ebe0a2f657fc4b20bb22b30156709a480
lazynormz <skupi1234@gmail.com>
2022-08-11 14:33:16 +0200

    Fixing pathing

--- src/http-common.tsx
+++ src/http-common.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-    baseURL: `185.51.76.204:8090/`,
+    baseURL: `http://185.51.76.204:8090/`,
     headers: {
         "Content-type": "application/json"
     }
