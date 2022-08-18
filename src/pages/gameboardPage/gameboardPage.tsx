55ddd08bea42105e890b5eb46d0fcf08a6d638cf
Frann0 <50768854+Frann0@users.noreply.github.com>
2022-08-18 10:37:31 +0200

    Revert "Revert "Merge branch 'Development' into main""
    This reverts commit 054cb629066a00d652e8ca0afa943c0374820d3c.

---
+++ src/pages/gameboardPage/gameboardPage.tsx
@@ -0,0 +1,59 @@
+import { observer } from 'mobx-react-lite'
+import React, { useState } from 'react'
+import Board from '../../components/gameBoard/board/board';
+import Player from '../../components/gameBoard/player/player';
+import Tiles from '../../components/gameBoard/tiles/tiles';
+import { useStore } from '../../stores/store';
+import './gameboardPage.scss'
+
+const GameboardPage = () => {
+    const [tasklistShown, setTasklistShown] = useState(false);
+    const [playersShown, setPlayersShown] = useState(false);
+    const { gameStore } = useStore();
+
+    const toggleTasklist = () => {
+        setTasklistShown(!tasklistShown);
+    }
+    const togglePlayers = () => {
+        setPlayersShown(!playersShown);
+    }
+
+    return (
+        <div className='Gameboard_Container'>
+            <div className='Gameboard_NavBackground'></div>
+            <div className='Gameboard_Wrapper'>
+                <div className={`Gameboard_TracklistContainer ${tasklistShown ? 'shown' : ''}`}>
+                    <div onClick={() => toggleTasklist()} className={`Gameboard_TracklistTitle ${tasklistShown ? 'shown' : ''}`}>{tasklistShown ? 'Tasklist' : 'T'}</div>
+                    <div className={`Gameboard_TracklistComponentContainer ${tasklistShown ? 'shown' : ''}`}>
+                        {gameStore.tiles.map((task: any) => (
+                            <Tiles tile={task} />
+                        ))}
+                    </div>
+                </div>
+
+                <div className='test'>
+                    <div className='test2'></div>
+                </div>
+
+                <div className='Gameboard_GameboardContainer'>
+                    <div className='Gameboard_GameboardWrapper'>
+                        <Board />
+                    </div>
+                </div>
+                <div className='test3'>
+                    <div className='test4'></div>
+                </div>
+                <div className={`Gameboard_PlayersContainer ${playersShown ? 'shown' : ''}`}>
+                    <div onClick={() => togglePlayers()} className={`Gameboard_PlayersTitle ${playersShown ? 'shown' : ''}`}>{playersShown ? 'Players' : 'P'}</div>
+                    <div className={`Gameboard_PlayersComponentContainer ${playersShown ? 'shown' : ''}`}>
+                        {gameStore.players.map((player: any) => (
+                            <Player player={player} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default observer(GameboardPage)
--- src/stores/gameStore.tsx
+++ src/stores/gameStore.tsx
@@ -1,14 +1,15 @@
 import { observable, makeAutoObservable, runInAction, toJS, action } from "mobx";
 import { HubConnection, HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
-import { GameRoom, Lobby } from "../models/game/gameInterfaces";
-import { UserDTO } from "../models/user/userInterface";
+import { GameRoom, Lobby, CloseLobbyDto, LeaveLobbyDto } from "../models/game/gameInterfaces";
+import { UserDTO, SimpleUser } from "../models/user/userInterface";
 import { useNavigate } from 'react-router-dom';
-import { lobbyPlayer } from "../models/player/playerInterface";
 
 export default class GameStore {
     @observable gameRoom: GameRoom | undefined;
     @observable lobby: Lobby | undefined;
-    @observable lobbyPlayers: lobbyPlayer[] = [];
+    @observable tiles: any[] = [{ id: 1, action: 'test action', to: 'test to', by: 'test by', shown: false }]
+    @observable players: any[] = [{ username: 'Test', nickname: 'Hovedskovasddasdas' }]
+    @observable lobbyPlayers: SimpleUser[] = [];
     hubConnection: HubConnection | null = null;
 
     constructor() {
@@ -17,7 +18,7 @@ export default class GameStore {
 
     createHubConnection = async () => {
         this.hubConnection = new HubConnectionBuilder()
-            .withUrl(process.env.REACT_GAME_SOCKET !== undefined ? process.env.REACT_GAME_SOCKET : "http://localhost:5121/game")
+            .withUrl(process.env.REACT_APP_GAME_SOCKET !== undefined ? process.env.REACT_APP_GAME_SOCKET : "http://localhost:5121/")
             .withAutomaticReconnect()
             .configureLogging(LogLevel.Information)
             .build();
@@ -40,23 +41,31 @@ export default class GameStore {
             });
         });
 
-        this.hubConnection.on('lobbyPlayerListUpdate', (players :lobbyPlayer[]) =>{
+        this.hubConnection.on('lobbyPlayerListUpdate', (players: SimpleUser[]) => {
             runInAction(() => {
                 this.lobbyPlayers = players;
             });
         })
+
+        this.hubConnection.on('lobbyClosed', () => {
+            runInAction(async () => {
+                this.lobby = undefined;
+                console.log('lobby is Closed');
+            });
+        });
+        return;
     }
 
     stopHubConnection = () => {
         this.hubConnection?.stop().catch(error => { });
     }
 
-    createLobby = async (userId: string) => {
+    createLobby = async (userId: string, func: Function) => {
         this.hubConnection?.invoke('CreateLobby', userId);
         this.hubConnection?.on('receiveLobby', async (lobby) => {
             runInAction(async () => {
                 this.lobby = await lobby;
-                console.log(this.lobby);
+                func()
             });
         });
     }
@@ -66,18 +75,26 @@ export default class GameStore {
         this.hubConnection?.invoke('client_StartGame')
     }
 
-    joinLobby = async (userId: string, lobbyPin: string) => {
+    joinLobby = async (userId: string, lobbyPin: string, func: Function) => {
         this.hubConnection?.invoke('JoinLobby', userId, lobbyPin)
-        this.hubConnection?.on('receiveLobby', async (lobby) => {
-            runInAction(async () => {
-                this.lobby = await lobby;
-                console.log(this.lobby);
-            });
+        this.hubConnection?.on('receiveLobby', async (lobby: Lobby) => {
+            this.lobby = await lobby;
+            func();
         });
+    }
+
+    closeLobby = async (lobbyId: string, hostId: string) => {
+        let cl: CloseLobbyDto = { lobbyID: lobbyId, hostID: hostId }
+        this.hubConnection?.invoke('CloseLobby', cl)
     }
 
     kickPlayer = async (userId: string) => {
         console.log(userId);
-
     }
+
+    leaveLobby = async (lobbyId: string, userId: string) => {
+        let ll: LeaveLobbyDto = { lobbyID: lobbyId, userID: userId }
+        this.hubConnection?.invoke('LeaveLobby', ll)
+    }
+
 }
