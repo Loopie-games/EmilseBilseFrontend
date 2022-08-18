7e6ed0784c6b4aaf33f8b31cd41730a7232bb144
Frann0 <50768854+Frann0@users.noreply.github.com>
2022-08-18 11:42:43 +0200

    Revert "JWT Header added to requests"
    This reverts commit f6094baea05d28db5f8607235c5e14020f210168.

--- src/http-common.tsx
+++ src/http-common.tsx
@@ -2,7 +2,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: process.env.REACT_APP_API !== undefined ? process.env.REACT_APP_API : "http://localhost:5121/",
     headers: {
-        "autherization": "Bearer " + localStorage.getItem("token"),
         "Content-type": "application/json"
     }
 });
