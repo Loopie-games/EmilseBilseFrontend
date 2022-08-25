ffabc5e606e1e851ef7259c388fd83af64633013
Frann0 <50768854+Frann0@users.noreply.github.com>
2022-08-25 12:26:38 +0200

    pop ups working

--- src/pages/test/testPage.tsx
+++ src/pages/test/testPage.tsx
@@ -1,30 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import Loader from '../../components/shared/loader/loader';
+import Popup from '../../components/shared/popups/popup';
 import { useStore } from '../../stores/store';
 import './testPage.scss'
 
 const TestPage = () => {
-  const [tile, setTile] = useState('');
-  const { tileStore, friendshipStore, userStore } = useStore();
-
-  const handleCreateTile = (username: string) => {
-    let addedByUserId = userStore.user?.id !== undefined ? userStore.user?.id : '';
-  }
-
-  const loadFriends = async () => {
-    await friendshipStore.getFriendList(userStore.user?.id ? userStore.user.id : '');
-  }
+  const [shouldShow, setShouldShow] = useState(false);
   useEffect(() => {
-    loadFriends();
-    console.log("asdasdasd");
-
-    console.log(friendshipStore._friendlist);
-
   }, [])
+
+  const t = false
+
+  const handleClose = () => {
+    setShouldShow(false);
+    console.log('closed');
+  }
+  const handleConfirm = () => {
+    setShouldShow(false);
+    console.log('confirmed');
+  }
+
+  const message = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo natus eos est sunt. Fugiat aperiam error recusandae perspiciatis est assumenda, officia dolor tempora sunt et? Blanditiis consequatur repellat voluptates corporis? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo natus eos est sunt. Fugiat aperiam error recusandae perspiciatis est assumenda, officia dolor tempora sunt et? Blanditiis consequatur repellat voluptates corporis? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo natus eos est sunt. Fugiat aperiam error recusandae perspiciatis est assumenda, officia dolor tempora sunt et? Blanditiis consequatur repellat voluptates corporis?'
+
 
   return (
     <>
-      <Loader />
+      <div className='Test_Container'>
+        <button onClick={() => setShouldShow(true)}>Show Popup</button>
+        {shouldShow ? <Popup isConfirmation={t} title="Title" errorMessage={message} handleClose={handleClose} handleConfirm={handleConfirm} /> : null}
+      </div>
     </>
   )
 }
