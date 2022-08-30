18d8d93bbb19c324f712fb4fdd8ced846a80794a
Frann0 <50768854+Frann0@users.noreply.github.com>
2022-08-30 11:20:11 +0200

    Revert "T"
    This reverts commit 15ae0c2edb01bdb59984b6f91941fe6484f74b86.

--- src/pages/tilesPages/tilepackCreatorPage/NewTilepackCreatorPage/newTilepackCreatorPage.tsx
+++ src/pages/tilesPages/tilepackCreatorPage/NewTilepackCreatorPage/newTilepackCreatorPage.tsx
@@ -1,23 +1,11 @@
 import React, { useState } from 'react'
 import Icon from '../../../../components/shared/icon/Icon'
-import SortableContainer from '../../../../components/tilepackCreator/sortableContainer'
-import SortableItem from '../../../../components/tilepackCreator/sortableItem'
 import './newTilepackCreatorPage.scss'
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
-
 
 const NewTilepackCreatorPage = () => {
     const [name, setName] = useState('')
     const [price, setPrice] = useState('')
     const [discount, setDiscount] = useState('')
-    const [items, setItems] = useState([{ id: "1", name: "Name" }])
-
-    const handleOnDragEnd = (result: any) => {
-        const i = Array.from(items);
-        const [reorderedItem] = i.splice(result.source.index, 1);
-        i.splice(result.destination.index, 0, reorderedItem);
-        setItems(i);
-    }
 
     return (
         <div className='NewTilePack_Container'>
@@ -47,37 +35,20 @@ const NewTilepackCreatorPage = () => {
                 {/* Tilepack creator side / Right side */}
                 <div className='NewTilePack_CreatorContainer'>
                     <div className='NewTilePack_CreatorWrapper'>
-                        <DragDropContext onDragEnd={handleOnDragEnd}>
-                            <div className='NewTilePack_TileContainer'>
-                                <div className='NewTilePack_CreatorTitle'>Available tiles</div>
-                                <Droppable droppableId='test1'>
-                                    {(provided) => (
-                                        <div className='NewTilePack_CreatorActionContainer' {...provided.droppableProps} ref={provided.innerRef}>
-                                            {items.map(({id, name}, index) => (
-                                                <Draggable key={id} draggableId={id} index={index}>
-                                                    {(provided) => (
-                                                        <div onClick={() => console.log(provided)} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}  >{name}</div>
-                                                    )}
-                                                </Draggable>
-                    
-                                            ))}
-                                            {provided.placeholder}
-
-                                            <div className='NewTilePack_NewTileButton'>
-                                                <Icon name="plus" />
-                                            </div>
-                                        </div>
-
-                                    )}
-                                </Droppable>
+                        <div className='NewTilePack_TileContainer'>
+                            <div className='NewTilePack_CreatorTitle'>Available tiles</div>
+                            <div className='NewTilePack_CreatorActionContainer'>
+                                action container
+                                <div className='NewTilePack_NewTileButton'>
+                                    <Icon name="plus" />
+                                </div>
                             </div>
-                            <div className='NewTilePack_TileContainer'>
-                                <div className='NewTilePack_CreatorTitle'>Selected tiles</div>
-                                <div className='NewTilePack_CreatorActionContainer'>action container</div>
-                            </div>
-                        </DragDropContext>
+                        </div>
+                        <div className='NewTilePack_TileContainer'>
+                            <div className='NewTilePack_CreatorTitle'>Selected tiles</div>
+                            <div className='NewTilePack_CreatorActionContainer'>action container</div>
+                        </div>
                     </div>
-
                     <div className='NewTilePack_CreatorButtonContainer'>
                         <div className='NewTilePack_CreatorButtonWrapper'>
                             <div className='NewTilePack_CancelButton'>Cancel</div>
