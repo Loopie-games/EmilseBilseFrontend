08cb0151954c87972b522772ecbd9f814ab37bbe
Frann0 <50768854+Frann0@users.noreply.github.com>
2022-09-12 12:30:27 +0200

    Toggle button on mobile nav menu

---
+++ src/stores/themeStore.tsx
@@ -0,0 +1,22 @@
+import { action, makeAutoObservable, observable } from "mobx";
+
+export class ThemeStore {
+
+    @observable theme: string | undefined;
+
+    @action
+    setTheme = () => {
+        document.documentElement.setAttribute('data-theme', localStorage.getItem('theme') ?? 'light');
+    }
+
+    @action
+    toggleTheme = () => {
+        this.theme = this.theme === 'light' ? 'dark' : 'light';
+        localStorage.setItem('theme', localStorage.getItem('theme') === 'light' ? 'dark' : 'light');
+        document.documentElement.setAttribute('data-theme', localStorage.getItem('theme')!);
+    }
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+}
