a5d30651c739993316fe99bb1452b56fd7caf53e
Frann0 <50768854+Frann0@users.noreply.github.com>
2022-09-19 15:26:35 +0200

    Updated loggedinbar with links section and changed stroke width on link svg

--- src/components/shared/loggedInBar/LoggedInBar.tsx
+++ src/components/shared/loggedInBar/LoggedInBar.tsx
@@ -12,12 +12,23 @@ const LoggedInBar = () => {
     const navigate = useNavigate();
     const url = window.location.pathname;
     const [isShown, setIsShown] = useState(false);
+    const [linksShown, setLinkShown] = useState(false);
     const [profileShown, setProfileShown] = useState(false);
     const [friendsShown, setFriendsShown] = useState(false);
     const [tilesShown, setTilesShown] = useState(false);
     const [settingsShown, setSettingsShown] = useState(false);
     const [logOutShown, setLogOutShown] = useState(false);
 
+
+
+    const linkSublinks = [
+        { name: 'Shop', link: `/shop`, iconName: 'link' },
+        { name: 'About us', link: `/aboutus`, iconName: 'link' },
+        { name: 'Contact us', link: `/contactus`, iconName: 'link' },
+        { name: 'FAQ', link: `/faq`, iconName: 'link' },
+        { name: 'Terms of use', link: `/terms`, iconName: 'link' },
+        { name: 'Privacy policy', link: `/privacy`, iconName: 'link' },
+    ];
     const profileSubLinks = [{ name: 'Your Profile', link: `/user/profile/${userStore.user?.id}`, iconName: 'profile' }];
     const friendlistSubLinks = [{ name: 'Friendlist', link: `/user/friendlist/${userStore.user?.id}`, iconName: 'friendslist' }, { name: 'Add Friend', link: `/user/addfriend/`, iconName: 'add_friend' }, { name: 'Friend Requests', link: `/user/friendrequests/`, iconName: 'friend_requests' }];
     const tileSubLinks = [
@@ -42,49 +53,63 @@ const LoggedInBar = () => {
         return t;
     
     }
+    const handleLinksClick = () => {
+        !isShown && setIsShown(true);
+        setLinkShown(!linksShown);
+        setProfileShown(false);
+        setFriendsShown(false);
+        setTilesShown(false);
+        setSettingsShown(false);
+        setLogOutShown(false);
+    }
 
     const handleProfileClick = () => {
         !isShown && setIsShown(true);
         setProfileShown(!profileShown);
+        setLinkShown(false);
         setFriendsShown(false);
         setTilesShown(false);
         setSettingsShown(false);
         setLogOutShown(false);
     }
     const handleFriendsClick = () => {
-        !isShown ? setIsShown(true) : () => { };
+        !isShown && setIsShown(true);
         setFriendsShown(!friendsShown);
         setProfileShown(false);
         setTilesShown(false);
         setSettingsShown(false);
         setLogOutShown(false);
+        setLinkShown(false);
     }
     const handleTilesClick = () => {
-        !isShown ? setIsShown(true) : () => { };
+       !isShown && setIsShown(true);
         setTilesShown(!tilesShown);
         setProfileShown(false);
         setFriendsShown(false);
         setSettingsShown(false);
         setLogOutShown(false);
+        setLinkShown(false);
     }
     const handleSettingsClick = () => {
-        !isShown ? setIsShown(true) : () => { };
+       !isShown && setIsShown(true);
         setSettingsShown(!settingsShown);
         setProfileShown(false);
         setFriendsShown(false);
         setTilesShown(false);
         setLogOutShown(false);
+        setLinkShown(false);
     }
     const handleSettings = () => {
         navigate('/user/settings')
     }
     const handleLogOutClick = () => {
-        !isShown ? setIsShown(true) : () => { };
+       !isShown && setIsShown(true);
         setLogOutShown(!logOutShown);
         setProfileShown(false);
         setFriendsShown(false);
         setTilesShown(false);
         setSettingsShown(false);
+        setLinkShown(false);
     }
     const handleLogOut = () => {
         userStore.logout();
@@ -98,6 +123,7 @@ const LoggedInBar = () => {
         setTilesShown(false);
         setSettingsShown(false);
         setLogOutShown(false);
+        setLinkShown(false);
     }
 
 
@@ -106,6 +132,24 @@ const LoggedInBar = () => {
             {checkIfBlacklistedRoute(url) ? null :
                 <>
                     <div className={`LoggedInBar-Container ${isShown ? 'shown' : ''}`}>
+                    <div className={`LoggedInBar-Wrapper ${linksShown ? 'asdasdasd ' : ''}`} onClick={handleLinksClick}>
+                            <div className={`LoggedInBar-ComponentTitle ${isShown ? 'shown' : ''} ${linksShown ? 'activated' : ''}`}>
+                                <div className='LoggedInBar-ComponentTitleIcon'><Icon name="link" /></div>
+                                <div className='LoggedInBar-ComponentTitleText shown'>Links</div>
+                            </div>
+                            <div className={`LoggedInBar-ComponentContainer ${linksShown ? 'asdasd' : ''}`}>
+                                {
+                                    linkSublinks.map((subLink, index) => {
+                                        return (
+                                            <div className={`LoggedInBar-ComponentTitle ${linksShown ? 'shown' : ''} ${url === subLink.link ? 'activated' : ''}`} key={index} onClick={() => { setIsShown(!isShown); navigate(subLink.link); console.log(isShown) }}>
+                                                <div className='LoggedInBar-ComponentTitleIcon'><Icon name={subLink.iconName} /></div>
+                                                <div className='LoggedInBar-SubComponentTitleText shown'>{subLink.name}</div>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
                         <div className={`LoggedInBar-Wrapper ${profileShown ? 'asdasdasd ' : ''}`} onClick={handleProfileClick}>
                             <div className={`LoggedInBar-ComponentTitle ${isShown ? 'shown' : ''} ${profileShown ? 'activated' : ''}`}>
                                 <div className='LoggedInBar-ComponentTitleIcon'><Icon name="profile" /></div>
