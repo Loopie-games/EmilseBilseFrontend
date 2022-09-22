42ceb3854c4116773541c3a187a131709b6c7d70
Frann0 <50768854+Frann0@users.noreply.github.com>
2022-09-22 15:20:32 +0200

    Profilepage Mobile

--- src/pages/profilePage/profilePageMobile/profilePageMobile.tsx
+++ src/pages/profilePage/profilePageMobile/profilePageMobile.tsx
@@ -4,10 +4,16 @@ import './profilePageMobile.scss'
 import testBanner from '../../../assets/Shared/pathethic.png'
 import { useStore } from '../../../stores/store'
 import { UserDTO } from '../../../models/user/userInterface'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import Filter from '../../../components/shared/filter/filter'
+import filterService from '../../../services/filterService'
+import ProfileFriends from '../../../components/profile/friends/profileFriends'
+import Loader from '../../../components/shared/loader/loader'
+import ProfileTiles from '../../../components/profile/tiles/profileTiles'
+import ProfileAchievements from '../../../components/profile/achievements/profileAchievements'
 
 const ProfilePageMobile = () => {
-    const { userStore, popupStore } = useStore();
+    const { userStore, popupStore, tileStore, friendshipStore } = useStore();
     const params = useParams();
     const [user, setUser] = useState<UserDTO>();
     const [isOwner, setIsOwner] = useState(false);
@@ -16,57 +22,192 @@ const ProfilePageMobile = () => {
     const [newNickname, setNewNickname] = useState("");
     const [showing, setShowing] = useState('overview');
     const defaultPic = 'https://as2.ftcdn.net/v2/jpg/02/15/84/43/1000_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg'
-
+    const navigate = useNavigate();
+    const [testPB, setTestPB] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [filtered, setFiltered] = useState<any[]>([]);
+    const [placeholder, setPlaceholder] = useState('');
+    const defaultImage = 'https://github.githubassets.com/images/modules/profile/achievements/yolo-default.png';
+    
+    const [testAchievements, setTestAchievements] = useState<any[]>([
+        {
+            achievement: {
+                id: 1,
+                title: "First achievement",
+                description: "First achievement description",
+            }
+        },
+        {
+            achievement: {
+                id: 2,
+                title: "Second achievement",
+                description: "Second achievement description",
+            }
+        },
+        {
+            achievement: {
+                id: 3,
+                title: "Third achievement",
+                description: "Third achievement description",
+            }
+        },
+    ]);
     let testFriends = 207;
 
     useEffect(() => {
+
+
         if (userStore.user?.id === params.id) {
-            setUser(userStore.user);
-            console.log(user?.nickname);
-            
             setIsOwner(true);
+            setUser(userStore.user);
         } else {
+            setIsOwner(false);
             getUser();
         }
-        return () => {
-            setUser(undefined);
-            setIsOwner(false);
-            setIsInEditMode(false);
+
+        if (showing === 'friends') {
+            getFriendList();
         }
-    }, [])
+
+        const loadFriends = async () => {
+            await friendshipStore.getFriendList(params.id!);
+        }
+        const loadTiles = async () => {
+            await tileStore.getTilesAboutUser(params.id!);
+        }
+        loadTiles();
+        loadFriends();
+
+        return () => {
+            setIsOwner(false);
+            setUser(undefined);
+        }
+
+    }, [params.id])
 
     useEffect(() => {
-        console.log(newNickname);
-    }, [newNickname])
-        
+        console.log(tileStore.tilesAboutUser?.length);
+
+
+    }, [tileStore.tilesAboutUser])
 
     const getUser = async () => {
         const user = await userStore.getUserById(params.id!);
         setUser(user);
     }
 
+    const getFriendList = async () => {
+        setFiltered([]);
+        setLoading(true);
+        setShowing('friends');
+        if (friendshipStore._friendlist === undefined) {
+            await friendshipStore.getFriendList(params.id!);
+        } else {
+            setFiltered(friendshipStore._friendlist!);
+        }
+        setLoading(false);
+        console.log(friendshipStore._friendlist);
+    }
+
+    const getTilesAboutUser = async () => {
+        setFiltered([]);
+        setLoading(true);
+        setShowing('tiles');
+        if (tileStore.tilesAboutUser?.length === undefined) {
+            await tileStore.getTilesAboutUser(params.id!);
+        } else {
+            setFiltered(tileStore.tilesAboutUser!);
+        }
+        setLoading(false);
+    }
+
+    const getAchievements = async () => {
+        setFiltered([]);
+        setLoading(true);
+        setShowing('achievements');
+
+        // await userStore.getAchievements(params.id!);
+        if (testAchievements.length === 0) {
+            //await userStore.getAchievements(params.id!);
+        } else {
+            setFiltered(testAchievements);
+        }
+        setLoading(false);
+    }
+
+
     const edit = () => {
         if (isInEditMode) {
-            let oldNickname = nickname;
             //TODO save changes when endpoint is done
             setIsInEditMode(false);
-            popupStore.showConfirmation(
-                "Are you sure?", 
-            ` You're about to change your nickname to ${newNickname}. Are you sure you want to do that ?`,
-            () => {
-                console.log("yes");
-                setNickname(newNickname!);
-            },
-            () => {
-                console.log("no");
-                setNickname(oldNickname!);
-            }
-            )
-            
         } else {
             setIsInEditMode(true);
         }
     }
+
+
+
+    const uploadProfilePic = async (event: any) => {
+        const file = event[0]
+        const response = await userStore.updateProfilePic(file);
+        setTestPB(response);
+    }
+
+    const filterInAchievements = (query: string) => {
+        if (testAchievements !== undefined) {
+            setFiltered(filterService.filterInAchievements(query, testAchievements));
+        }
+    }
+
+    const filterInFriends = (query: string) => {
+        if (friendshipStore._friendlist !== undefined) {
+            setFiltered(filterService.filterForFriends(query, friendshipStore._friendlist));
+        }
+        console.log('====================================');
+        console.log(filtered);
+        console.log('====================================');
+    }
+
+    const filterInTiles = (query: string) => {
+        if (tileStore.tilesAboutUser !== undefined) {
+            setFiltered(filterService.filterForTiles(query, tileStore.tilesAboutUser));
+        }
+    }
+
+    const filter = (query: string) => {
+
+        switch (showing) {
+            case 'achievements':
+                filterInAchievements(query);
+                break;
+            case 'friends':
+                filterInFriends(query);
+                break;
+            case 'tiles':
+                filterInTiles(query);
+                break;
+            default:
+                break;
+        }
+    }
+
+    const getPlaceholderText = () => {
+        switch (showing) {
+            case 'achievements':
+                return 'Search for achievements';
+            case 'friends':
+                return 'Search for friends';
+            case 'tiles':
+                return 'Search for tiles';
+            default:
+                return '';
+        }
+    }
+
+    useEffect(() => {
+        filter('');
+        setPlaceholder(getPlaceholderText());
+    }, [showing, friendshipStore._friendlist, tileStore.tilesAboutUser])
 
     return (
         <div className='ProfilePageM_Container'>
@@ -88,66 +229,99 @@ const ProfilePageMobile = () => {
                             <input type='text' placeholder='Username' value={user?.username} disabled={true} />
                         </div>
                     </div>
-                    {isOwner && 
-                    <div className='ProfilePageM_ButtonContainer'>
-                        <div className='ProfilePageM_EditButton' onClick={edit}>    
-                            {isInEditMode ? "Save Changes" : "Edit User"}
+                    {isOwner &&
+                        <div className='ProfilePageM_ButtonContainer'>
+                            <div className='ProfilePageM_EditButton' onClick={edit}>
+                                {isInEditMode ? "Save Changes" : "Edit User"}
+                            </div>
                         </div>
-                    </div>
                     }
                 </div>
                 <div className='ProfilePageM_UserBio'>
-                    <input id='Bio' type="text"  disabled={!isInEditMode}/>
+                    <input id='Bio' type="text" disabled={!isInEditMode} />
                 </div>
                 <div className='ProfilePageM_AchievementContainer'>
-                <div className='ProfilePageM_FriendsContainer'>
-                    <div className='ProfilePageM_FriendsTitle'>Friends - {testFriends}</div>
+                    <div className='ProfilePageM_FriendsContainer'>
+                        <div className='ProfilePageM_FriendsTitle'>Friends - {friendshipStore._friendlist?.length}</div>
                         <div className='ProfilePageM_FriendsContentContainer'>
-                        <div className='ProfilePageM_FriendsContent'>
-                                <img src={defaultPic} alt="" />
-                            </div>
-                            <div className='ProfilePageM_FriendsContent'>
-                                <img src={defaultPic} alt="" />
-                            </div>
-                            <div className='ProfilePageM_FriendsContent'>
-                                <img src={defaultPic} alt="" />
-                            </div>
+
+                            {friendshipStore._friendlist?.slice(0, 3).map((friend, index) => (
+                                <div className='ProfilePageM_FriendsContent'>
+                                    <img src={defaultPic} alt="" onClick={() => navigate('/user/profile/' + friend.user.id)} />
+                                </div>
+                            ))}
                         </div>
                     </div>
                     <div className='ProfilePageM_FriendsContainer'>
-                    <div className='ProfilePageM_FriendsTitle'>Title</div>
+                        <div className='ProfilePageM_FriendsTitle'>Achievements - {testAchievements.length}</div>
                         <div className='ProfilePageM_FriendsContentContainer'>
-                        <div className='ProfilePageM_FriendsContent'>
-                                <img src={defaultPic} alt="" />
-                            </div>
-                            <div className='ProfilePageM_FriendsContent'>
-                                <img src={defaultPic} alt="" />
-                            </div>
-                            <div className='ProfilePageM_FriendsContent'>
-                                <img src={defaultPic} alt="" />
-                            </div>
+                            {testAchievements.slice(0, 3).map((achievement, index) => (
+                                <div className='ProfilePageM_FriendsContent'>
+                                    <img src={defaultImage} alt="" />
+                                </div>
+                            ))}
                         </div>
-                        
+
                     </div>
                 </div>
                 <div className='ProfilePageM_ContentContainer'>
                     <div className='ProfilePageM_ContentWrapper'>
                         <div className='ProfilePageM_NavContainer'>
-                            <div className={`ProfilePageM_NavItem ${showing ==='overview' ? 'active' : ''}`}  onClick={() => setShowing('overview')}>Overview</div>
-                            <div className={`ProfilePageM_NavItem ${showing ==='achievements' ? 'active' : ''}`} onClick={() => setShowing('achievements')}>Achievements</div>
-                            <div className={`ProfilePageM_NavItem ${showing ==='friends' ? 'active' : ''}`} onClick={() => setShowing('friends')}>Friends</div>
-                            <div className={`ProfilePageM_NavItem ${showing ==='tiles' ? 'active' : ''}`} onClick={() => setShowing('tiles')}>Tiles</div>
+                            <div className={`ProfilePageM_NavItem ${showing === 'overview' ? 'active' : ''}`} onClick={() => setShowing('overview')}>
+                                Overview
+                            </div>
+                            <div className={`ProfilePageM_NavItem ${showing === 'achievements' ? 'active' : ''}`} onClick={() => getAchievements()}>
+                                Achievements
+                            </div>
+                            <div className={`ProfilePageM_NavItem ${showing === 'friends' ? 'active' : ''}`} onClick={() => getFriendList()}>
+                                Friends
+                            </div>
+                            <div className={`ProfilePageM_NavItem ${showing === 'tiles' ? 'active' : ''}`} onClick={() => getTilesAboutUser()}>
+                                Tiles
+                            </div>
                         </div>
                         <div className='ProfilePageM_Content'>
+                            {showing !== 'overview' &&
+                                <div className='ProfilePage_FilterContainer'>
+                                    <div className='ProfilePage_FilterWrapper'>
+                                        <Filter filter={(e: string) => filter(e)} placeholder={placeholder} />
+                                    </div>
+                                </div>
+                            }
                             {showing === 'overview' && <div>Overview</div>}
-                            {showing === 'achievements' && <div>Achievements</div>}
-                            {showing === 'friends' && <div>Friends</div>}
-                            {showing === 'tiles' && <div>Tiles</div>}
+                            {showing === 'achievements' && <>
+                                {loading ? <Loader /> :
+                                    <div className='ProfilePageM_cContainer'>
+                                        {filtered.map((Achievements) => (
+                                            <ProfileAchievements key={Achievements.achievement.id} {...Achievements} />
+                                        ))}
+                                    </div>
+                                }
+                            </>}
+                            {showing === 'friends' && <>
+                                {loading ? <Loader /> :
+                                    <div className='ProfilePageM_cContainer'>
+                                        {filtered.map((friend) => (
+                                            <ProfileFriends key={friend.id} {...friend} />
+                                        ))}
+                                    </div>
+                                }
+                            </>}
+                            {showing === 'tiles' &&
+                                <>
+                                    {loading ? <Loader /> :
+                                        <div className='ProfilePageM_cContainer'>
+                                            {filtered.map((tiles) => (
+                                                <ProfileTiles key={tiles.id} {...tiles} />
+                                            ))}
+                                        </div>
+                                    }
+                                </>}
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
