fcc01b98fe9d4d6e7ab1a97b3e4a55633a12fc4d
ceci6182 <ceci6182@easv365.dk>
2022-11-02 11:22:01 +0100

    ffa

--- src/stores/lobbyStore.tsx
+++ src/stores/lobbyStore.tsx
@@ -107,4 +107,14 @@ export default class LobbyStore {
         return response.data
     }
 
+    @action
+    startFFA = async (cDto:CreateGameDto) => {
+        if (this.lobby === undefined) throw new Error("Game cannot be created without a lobby")
+        if (this.players.length < 2) throw new Error("You need to be at least to players to start a game")
+        const response = await lobbyService.startFFA(cDto);
+        let gameId = response.data.id
+        await this.hubConnection!.invoke('StartGame', this.lobby.id, gameId)
+        return response.data
+    }
+
 }
